462b7d5d220de707a10d49157ab36ecfe798d7bc
Alexa Cristina <ac@mysite.org>
2020-02-12 23:50:21 +0200

    Updates

---
+++ precache-manifest.76537e1ff431c0168c8e771c6391de50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e5c34236e8a9250782233d69c65ff1",
+    "url": "/react-robo/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/react-robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f2fd5845c83f170363a6",
+    "url": "/react-robo/static/css/main.47587586.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/react-robo/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-robo/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2fd5845c83f170363a6",
+    "url": "/react-robo/static/js/main.6bfd156d.chunk.js"
+  },
+  {
+    "revision": "3342646c359e8fa9cf90",
+    "url": "/react-robo/static/js/runtime-main.f208cc27.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
