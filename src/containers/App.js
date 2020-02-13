a58b4e01a22cbe431c2a99a0a8f8bc9247dc31aa
Alexa Cristina <ac@mysite.org>
2020-02-14 00:50:25 +0200

    Adding Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+} 
 class App extends Component {
 
     constructor() {
         super()
         this.state = {
-            robots: [],
-            searchField: ''
+            robots: []
         }
     }
 
@@ -21,13 +34,9 @@ class App extends Component {
         .then(users => this.setState({ robots: users }));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
-        
-    }
-
     render() {
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
@@ -35,7 +44,7 @@ class App extends Component {
             <h1>Loading</h1> :
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filteredRobots}/>
@@ -46,4 +55,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>, document.getElementById('root'));                ;
 
 serviceWorker.register();
